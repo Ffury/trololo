0bc0bbfc46c829c975bdcfbfe4b8612294f2bfea
Valery <drozdllera@gmail.com>
2020-06-29 21:47:39 +0300

    first commit

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,21 @@
+#include <array>
+#include <locale.h>
+#include <iostream>
+#include <Windows.h>
+#include <string_view>
+#include <tuple>
+#include <type_traits>
+
+using namespace std;
+
+int main()
+ {
+	setlocale(LC_ALL, " ");
+// 	SetConsoleCP(1251);
+// 	SetConsoleOutputCP(1251);
+	string s;
+	gets_s;
+	cin >> s;
+	cout << s;
+	
+}
